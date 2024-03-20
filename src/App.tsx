70eea57f831e388932f62b13cf616ac50cc7d8d1
victorhelopes <victorhelopes@gmail.com>
2024-03-20 11:21:08 -0300

    add: material icons

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import store from './store/index.tsx';
 function App() {
   return (
     <Provider store={store}>
+      <link href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp" rel="stylesheet"/>
+    
       <Home/>
     </Provider>
   );
